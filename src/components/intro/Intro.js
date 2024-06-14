3726923ddb1cb3cf34fcffdb2586dee43d03fd63
GetnetAm <getnetamsalu2119@gmail.com>
2024-06-14 09:50:08 -0700

    home page design

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -3,6 +3,16 @@ import "./intro.css"
 import Github from "../../img/github.png"
 import LinkedIn from "../../img/linkedin.png"
 import Instagram from "../../img/instagram.png"
+import Vector1 from "../../img/Vector1.png"
+import Vector2 from "../../img/Vector2.png"
+import boy from "../../img/boy.png"
+import Crown from "../../img/crown.png"
+import glassesImoji from "../../img/glassesimoji.png"
+import gc1 from "../../img/gc.jpg"
+import gc2 from "../../img/gc2.jpg"
+import gcone from "../../img/gcone.svg"
+import FloatingDev from "../floatingDiv/FloatingDev"
+import thumbup from '../../img/thumbup.png'
 
 function Intro() {
   return (
@@ -31,7 +41,33 @@ function Intro() {
         </div>
 
         <div className="i-right">
-            I am right hire
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src={gc2} alt="" className="gc_image" />
+        <img src={glassesImoji} alt="" />
+        <div style={{top: '-4%', left:'68%'}}>
+            <FloatingDev image={thumbup} txt1="Best Design" txt2="Award"/>
+
+        </div>
+
+        <div style={{top: "18rem", left:"0rem"}}>
+        <FloatingDev image={Crown} txt1="Web" txt2="Developer"/>
+        </div>
+
+        <div className="blur" 
+        style={{
+            background:"#c1f5ff",
+            top: "17rem",
+            width:"21rem",
+            height:"11rem",
+            left:"-9rem"
+
+            }}>
+
+
+
+        </div>
+
 
         </div>
       
